715c5720db0e39625f44ccdc3348acc5e8c5b7b8
Vokan Kocak <vokankocak@gmail.com>
2020-07-27 17:13:26 +0200

    LET+CONST

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var Go = console.log;
 
-// LET
+// <---------------------LET------------------->
 
 /*function ville(){
     let favoriteCityId ='rome';
@@ -19,14 +19,87 @@ favoriteCityId ="paris";
 Go(favoriteCityId);
 
 
-// CONST:
+// <-----------------CONST: ----------------->
 
 const citiesId=["paris","nyc","rome","rio-de-janeiro"];
     Go(citiesId);
 
 // citiesId =[];
-   // Go(citiesId); // --> une erreur se produit quand on compile.
+  // Go(citiesId); // --> une erreur se produit quand on compile.
 
 citiesId.push("tokyo");
 Go(citiesId);
 
+//<--------Création d'objet-------------->
+
+function getWeather(cityId){
+    let city = cityId.toUpperCase(); 
+    let temperature = 20;
+    return { city, temperature };
+}
+const weather = getWeather(favoriteCityId);
+Go(weather);
+
+//<---------Affectation destructurée---------->
+
+const{ city,temperature } = weather;
+Go(city);
+Go(temperature);
+
+//<-----------Rest Operator----------->
+const [parisId, nycId, ...othersCitiesId] = citiesId;
+Go(parisId);
+Go(nycId);
+Go(othersCitiesId.length);
+
+//<----------Classes----------->
+
+class Trip {
+    constructor(id,name,imageUrl){
+        this.id=id;
+        this.name=name;
+        this.imageUrl=imageUrl
+    }
+
+    get price(){
+        return this._price;
+    }
+
+    set price(newPrice){
+        this._price=newPrice;
+    }
+
+    toString(){
+        return "Trip ["+this.id +" "+ this.name +" "+ this.imageUrl +" "+ this._price +"]";
+    }
+
+    static getDefaultTrip(){
+        return new Trip ("rio-de-janeiro","Rio de Janeiro","img/rio-de-janeiro.jpg");
+    }
+}
+    let parisTrip = new Trip('paris','Paris','img/paris.jpg');
+    parisTrip.price=100;
+
+    const defaultTrip =  Trip.getDefaultTrip();
+
+    Go(parisTrip.toString());
+    Go(parisTrip.name);
+    Go(defaultTrip.toString());
+
+
+    //<---------Heritage--------->
+
+class FreeTrip extends Trip {
+
+    toString(){
+        return "Free" + "Trip ["+this.id +" "+ this.name +" "+ this.imageUrl +" "+ this._price +"]";
+    }
+}
+    const freeTrip = new FreeTrip("nantes","Nantes"," img/nantes.jpg");
+    freeTrip.price = 0;
+
+    Go(freeTrip.toString());
+
+    //<-----------Promise,Set,Map,Arrow function---------->
+
+    
